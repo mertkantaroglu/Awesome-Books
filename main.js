f041889874c7ff40d78e9c0045d0fb0e14d55294
mertkantaroglu <mertkantaroglu@gmail.com>
2023-01-18 22:30:21 +0300

    update styling and fix linter errors

--- main.js
+++ main.js
@@ -84,8 +84,6 @@ Book.displayBooks();
 populateFields();
 
 // Nav Bar
-
-const navAwesome = document.querySelector(".navAwesome");
 const navList = document.querySelector(".navList");
 const navAdd = document.querySelector(".navAdd");
 const navContact = document.querySelector('.navContact');
@@ -94,27 +92,20 @@ const addBookPage = document.querySelector('.add-book-page');
 const contact = document.querySelector('.contact');
 
 
-
-navAwesome.addEventListener('click',() => {
-  infoPage.classList.remove("close");
-addBookPage.classList.remove("close");
-contact.classList.remove("close");
-});
-
 navList.addEventListener('click',() => {
   infoPage.classList.remove("close");
-addBookPage.classList.add("close");
-contact.classList.add("close");
+  addBookPage.classList.add("close");
+  contact.classList.add("close");
 });
 
 navAdd.addEventListener('click',() => {
   infoPage.classList.add("close");
-addBookPage.classList.remove("close");
-contact.classList.add("close");
+  addBookPage.classList.remove("close");
+  contact.classList.add("close");
 });
 
 navContact.addEventListener('click',() => {
   infoPage.classList.add("close");
-addBookPage.classList.add("close");
-contact.classList.remove("close");
+  addBookPage.classList.add("close");
+  contact.classList.remove("close");
 });

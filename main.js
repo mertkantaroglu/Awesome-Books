ca39bdbdee157ef7a02a5dc809ef35b50cadbfb8
otmanTR <r.brk.otman@gmail.com>
2023-01-18 16:05:53 +0300

    js updated

--- main.js
+++ main.js
@@ -82,3 +82,39 @@ form.addEventListener('submit', (e) => {
 
 Book.displayBooks();
 populateFields();
+
+// Nav Bar
+
+const navAwesome = document.querySelector(".navAwesome");
+const navList = document.querySelector(".navList");
+const navAdd = document.querySelector(".navAdd");
+const navContact = document.querySelector('.navContact');
+const infoPage= document.querySelector('.info-page');
+const addBookPage = document.querySelector('.add-book-page');
+const contact = document.querySelector('.contact');
+
+
+
+// navAwesome.addEventListener('click',() => {
+// infoPage.classList.add("show");
+// addBookPage.classList.add("show");
+// contact.classList.add("show");
+// });
+
+navList.addEventListener('click',() => {
+  infoPage.classList.add("show");
+addBookPage.classList.add("close");
+contact.classList.add("close");
+});
+
+navAdd.addEventListener('click',() => {
+  infoPage.classList.add("close");
+addBookPage.classList.add("show");
+contact.classList.add("close");
+});
+
+navContact.addEventListener('click',() => {
+  infoPage.classList.add("close");
+addBookPage.classList.add("close");
+contact.classList.add("show");
+});
